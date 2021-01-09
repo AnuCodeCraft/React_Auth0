5c989249d0ab37b37318f748bed469d35061b48c
Anupriya Jha <anupriya10031@gmail.com>
2021-01-09 08:34:48 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import LoginButton from  "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <LoginButton/>
+      <LogoutButton/>
+      
+    </>
+  );
+}
+
+export default App;
